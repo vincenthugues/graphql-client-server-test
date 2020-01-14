5b0130599ecadb9ec8b418247795ceb7a34c4405
Vincent Hugues <vincenthugues.dev@gmail.com>
2020-01-15 00:01:53 +0100

    feat(client): handle editItem mutation

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { useMutation, useQuery } from 'react-apollo';
 import { Link } from 'react-router-dom';
 
 const ItemQuery = gql`
@@ -14,32 +14,48 @@ const ItemQuery = gql`
   }
 `;
 
-const Item = ({ match: { params: { id } } }) => (
+const editItemMutation = gql`
+  mutation editItem($id: String!, $name: String, $price: Float, $stock: Int) {
+    editItem(id: $id, name: $name, price: $price, stock: $stock) {
+      id
+      name
+      price
+      stock
+    }
+  }
+`;
+
+const ItemView = ({ item: { id, name, price, stock }, onIncreaseStock, onDecreaseStock }) => (
   <Fragment>
     <h2>Item</h2>
-    <Query query={ItemQuery} variables={{ id }}>
-      {({ loading, error, data }) => {
-        if (loading) return <h4>Loading...</h4>;
-        if (error) {
-          console.log(error.message);
-          return <h4>Unable to get data for this item</h4>;
-        }
-
-        const {
-          id,
-          name,
-          price,
-          stock,
-        } = data.item;
-
-        return (
-          <div key={id}> #{id} <b>{name}</b> - {price} ({stock}) </div>
-        );
-      }}
-    </Query>
+    <div>#{id} <b>{name}</b> - {price} ({stock})</div>
+    <br />
+    <div>
+      <input type="button" onClick={onIncreaseStock} value="Stock+" />
+      <input type="button" onClick={onDecreaseStock} value="Stock-" />
+    </div>
     <br />
     <Link to="/items">Back to items</Link>
   </Fragment>
 );
+
+const Item = ({ match: { params: { id } } }) => {
+  const { loading: queryLoading, error: queryError, data } = useQuery(ItemQuery, { variables: { id } });
+  const [ editItem, { loading: mutationLoading, error: mutationError } ] = useMutation(editItemMutation);
+  const onUpdateStock = async stock => editItem({ variables: { id, stock } });
+
+  if (queryLoading || mutationLoading) return <h4>Loading...</h4>;
+  if (queryError) return <h4>Unable to get data for this item: {queryError.message}</h4>;
+  if (mutationError) return <h4>Unable to update this item: {mutationError.message}</h4>;
+
+  const { item } = data;
+  return (
+    <ItemView
+      item={item}
+      onIncreaseStock={() => onUpdateStock(item.stock + 1)}
+      onDecreaseStock={() => onUpdateStock(item.stock - 1)}
+    />
+  );
+};
 
 export default Item;
